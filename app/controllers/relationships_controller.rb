631cf1ee283b98c6b689a9f923a45f587b44a06a
viettranisnottaken <tranxuanviet1997@gmail.com>
2019-07-02 21:10:17 +0700

    chap 14 done + ajax

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,13 +1,19 @@
 class RelationshipsController < ApplicationController
   def create
-    user = User.find(params[:followed_id])
-    current_user.active_relationships.create!(followed_id: user.id)
-    redirect_to user
+    @user = User.find(params[:followed_id])
+    current_user.active_relationships.create!(followed_id: @user.id)
+    respond_to do |format|
+      format.html {redirect_to @user}
+      format.js
+    end
   end
 
   def destroy
-    user = Relationship.find(params[:id]).followed
-    current_user.active_relationships.find_by(followed_id: user.id).destroy
-    redirect_to user
+    @user = Relationship.find(params[:id]).followed
+    current_user.active_relationships.find_by(followed_id: @user.id).destroy
+    respond_to do |format|
+      format.html {redirect_to @user}
+      format.js
+    end
   end
 end
